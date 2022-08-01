76bf00e4800337ed4c5269366cfa8ca0bda4488e
jwaine44 <jwaine44@gmail.com>
2022-08-01 15:53:06 -0400

    Initialize Repo

---
+++ Human.cs
@@ -0,0 +1,37 @@
+public class Human
+{
+    public string Name;
+    public int Strength;
+    public int Intelligence;
+    public int Dexterity;
+    public int Health;
+
+    // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+    public Human(string val)
+    {
+        Name = val;
+        Strength = 3;
+        Intelligence = 3;
+        Dexterity = 3;
+        Health = 100;
+    }
+    
+    // Add a constructor to assign custom values to all fields
+    public Human(string val, int strengthNum, int intelligenceNum, int dexNum, int healthNum)
+    {
+        Name = val;
+        Strength = strengthNum;
+        Intelligence = intelligenceNum;
+        Dexterity = dexNum;
+        Health = healthNum;
+    }
+    
+    // Build Attack method
+    Human attacker = new Human("Benny");
+    public int Attack(Human target)
+    {
+        target.Health -= (attacker.Strength * 5);
+        return target.Health;
+    }
+}
+
